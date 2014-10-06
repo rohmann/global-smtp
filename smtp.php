4fbdf2d0986201475e768f701998bf0ef1ec39c8
Alexander Rohmann <alexander@rohmann.us>
2014-10-06 15:34:06 -0400

    Full refactor. Add ability to disable per env.

--- smtp.php
+++ smtp.php
@@ -1,116 +1,158 @@
-<?php 
+<?php
 /*
 Plugin Name: Multisite SMTP
-Plugin URI: https://github.com/webtekk/wp-multisite-smtp
+Plugin URI: https://github.com/rohmann/wp-multisite-smtp
 Description: Allows for the setup of SMTP mail via constants in wp-config.php
 Author: Alexander Rohmann
-Author URI: https://github.com/webtekk/
-Version: 1.0
+Author URI: https://github.com/rohmann/
+Version: 2.0
+Text Domain: multisite-smtp
 */
 
-//Validate settings before hooking phpmailer
-if(global_setup_network_smtp_validate() ) {
-    add_action('phpmailer_init','global_setup_network_smtp');
-}
+/**
+ * Configures WordPress PHPMailer with values from wp-config.php
+ * Documentation: https://github.com/rohmann/wp-multisite-smtp
+ */
+class Multisite_SMTP {
 
-//Configure phpmailer using our constants
-function global_setup_network_smtp($phpmailer) {
+    /**
+     * I am a singleton
+     */
+    protected static $instance;
 
-    //debug?
-    if(defined('GLOBAL_SMTP_DEBUG') && GLOBAL_SMTP_DEBUG ) $phpmailer->SMTPDebug = true;
+    /**
+     * Store validation types
+     */
+    protected $validations;
 
-    //preset
-    $phpmailer->Mailer = "smtp";
-    $phpmailer->SMTPAuth = true;
-    
-    //required
-    $phpmailer->Host = GLOBAL_SMTP_HOST;
-    $phpmailer->Username = GLOBAL_SMTP_USER;
-    $phpmailer->Password = GLOBAL_SMTP_PASSWORD;
-    
-    //assumed
-    $phpmailer->From = GLOBAL_SMTP_FROM;
-    $phpmailer->FromName = GLOBAL_SMTP_FROM_NAME;
-    $phpmailer->Port = GLOBAL_SMTP_PORT;
-    $phpmailer->SMTPSecure = GLOBAL_SMTP_SECURE; 
-    $phpmailer->AuthType = GLOBAL_SMTP_AUTH_TYPE;
+    /**
+     * Checks settings, and hooks into phpmailer if everything is good.
+     */
+    function __construct() {
 
-    //Optional
-    $phpmailer->Sender = defined('GLOBAL_SMTP_RETURN_PATH') ? GLOBAL_SMTP_RETURN_PATH : GLOBAL_SMTP_FROM;
+        $this->prepare_settings();
+        $check = $this->validate();
 
-    if(defined('GLOBAL_SMTP_REPLYTO_FROM')) {
-        $phpmailer->AddReplyTo(GLOBAL_SMTP_REPLYTO_FROM, defined('GLOBAL_SMTP_REPLYTO_FROM_NAME') ? GLOBAL_SMTP_REPLYTO_FROM_NAME : GLOBAL_SMTP_FROM_NAME);    
-    }
-    
-}
-
-//If WP_DEBUG is enabled, this will perform strict validation and output debug messages otherwise this always returns true after assuming defaults
-function global_setup_network_smtp_validate() {
-
-    //Assume any undefined settings
-    $assume = array('GLOBAL_SMTP_PORT'=> 465,
-                        'GLOBAL_SMTP_SECURE' => 'ssl',
-                        'GLOBAL_SMTP_TIMEOUT' => 10,
-                        'GLOBAL_SMTP_FROM' => get_site_option('admin_email','',true),
-                        'GLOBAL_SMTP_FROM_NAME' => get_site_option('site_name','',true),
-                        'GLOBAL_SMTP_AUTH_TYPE' => 'LOGIN');
-
-    foreach ($assume as $setting => $default) {
-        if(!defined($setting)) {
-            define($setting, $default);
+        if( is_wp_error( $check ) ) {
+            trigger_error($check->get_error_message(), E_USER_WARNING);
+        } else {
+            add_action( 'phpmailer_init', array( $this, 'mailer') );
         }
+
+        unset($this->validations);
     }
-    
-    if(defined('WP_DEBUG') && WP_DEBUG ) {
-    
-        $error = false;
-        
-        //classify validations
-        $required = array('GLOBAL_SMTP_HOST','GLOBAL_SMTP_USER','GLOBAL_SMTP_PASSWORD');
-        $is_email = array('GLOBAL_SMTP_RETURN_PATH','GLOBAL_SMTP_FROM','GLOBAL_SMTP_REPLYTO_FROM');
-        $not_empty = array('GLOBAL_SMTP_FROM','GLOBAL_SMTP_FROM_NAME');
-        $is_int = array('GLOBAL_SMTP_PORT','GLOBAL_SMTP_TIMEOUT');
-        $should_be = array('GLOBAL_SMTP_SECURE' => array('ssl','tls','none'),
+
+    /**
+     * Self Instantiate
+     * Note: Plugin can be disable internally by setting GLOBAL_SMTP_DISABLE to true.
+     * This is useful if you have different needs between staging and production environments.
+     */
+    public static function launch() {
+        self::$instance = (defined('GLOBAL_SMTP_DISABLE') && GLOBAL_SMTP_DISABLE ) ? null : new self();
+    }
+
+    /**
+     * Prepare PHP Mailer settings. Allows for minimum configuration by asumming common defualts.
+     */
+    protected function prepare_settings() {
+        $this->validations = new stdClass;
+        $this->validations->required = array('GLOBAL_SMTP_HOST','GLOBAL_SMTP_USER','GLOBAL_SMTP_PASSWORD');
+        $this->validations->is_email = array('GLOBAL_SMTP_RETURN_PATH','GLOBAL_SMTP_FROM','GLOBAL_SMTP_REPLYTO_FROM');
+        $this->validations->not_empty = array('GLOBAL_SMTP_FROM','GLOBAL_SMTP_FROM_NAME');
+        $this->validations->is_int = array('GLOBAL_SMTP_PORT','GLOBAL_SMTP_TIMEOUT');
+        $this->validations->should_be = array('GLOBAL_SMTP_SECURE' => array('ssl','tls','none'),
             'GLOBAL_SMTP_AUTH_TYPE' => array('LOGIN','PLAIN','NTLM') );
 
-        //Run Validations
-        foreach ($required as $setting) {
+        //Assume any undefined settings
+        $assume = array(
+            'GLOBAL_SMTP_PORT'=> 465,
+            'GLOBAL_SMTP_SECURE' => 'ssl',
+            'GLOBAL_SMTP_TIMEOUT' => 10,
+            'GLOBAL_SMTP_FROM' => get_site_option('admin_email','',true),
+            'GLOBAL_SMTP_FROM_NAME' => get_site_option('site_name','WordPress',true),
+            'GLOBAL_SMTP_AUTH_TYPE' => 'LOGIN',
+        );
+
+        foreach ($assume as $setting => $default) {
             if(!defined($setting)) {
-                trigger_error($setting." is required for Multisite SMTP. Please define this in wp-config.php", E_USER_WARNING);
-                $error=true;
+                define($setting, $default);
             }
         }
-
-        foreach ($is_email as $email) {
-            if (defined($email) && !is_email(constant($email))) {
-                trigger_error("Value of ".$email." is not a valid email address. Check wp-config.php, or ensure a valid fallback is available.", E_USER_WARNING);
-                $error=true;
-            }
-        }
-
-        foreach ($not_empty as $key) {
-            if(defined($key) && constant($key)=="") {
-                trigger_error($key." is empty. Check wp-config.php, or ensure a valid fallback is available.", E_USER_WARNING);
-                $error=true;
-            }
-        }
-
-        foreach ($is_int as $key) {
-            if(defined($key) && !is_int(constant($key)) ) {
-                trigger_error($key." should be an integer. The force is not so strong with this one...", E_USER_WARNING);
-                $error=true;
-            }
-        }
-
-        foreach ($should_be as $key => $allowed) {
-            if(defined($key) && !in_array(constant($key), $allowed)) {
-                trigger_error($key." is invalid. It should be one of these values: '".implode("' , '",$allowed)."'", E_USER_WARNING);
-                $error=true;
-            }
-        }
-
-        if($error) return false;
     }
 
-    return true;
+    /**
+     * Validate Configuration to ensure things are setup correctly
+     * @return bool|WP_Error Returns true if successful, else WP_Error with message
+     */
+    protected function validate() {
+
+        foreach ($this->validations->required as $setting) {
+            if(!defined($setting)) {
+                return new WP_Error( 'multisite-smtp', sprintf( __( '%s is required for Multisite SMTP. Please define this in wp-config.php.', 'multisite-smtp' ), $setting ) );
+            }
+        }
+
+        foreach ($this->validations->is_email as $setting) {
+            if (defined($setting) && !is_email(constant($setting))) {
+                return new WP_Error( 'multisite-smtp', sprintf( __( 'Value of %s is not a valid email address. Check wp-config.php, or ensure a valid fallback is available.', 'multisite-smtp' ), $setting ) );
+            }
+        }
+
+        foreach ($this->validations->not_empty as $setting) {
+            if(defined($setting) && constant($setting)=="") {
+                return new WP_Error( 'multisite-smtp', sprintf( __( '%s  is empty. Check wp-config.php, or ensure a valid fallback is available.', 'multisite-smtp' ), $setting ) );
+            }
+        }
+
+        foreach ($this->validations->is_int as $setting) {
+            if(defined($setting) && !is_int(constant($setting)) ) {
+                return new WP_Error( 'multisite-smtp', sprintf( __( '%s should be an integer. The force is not so strong with this one...', 'multisite-smtp' ), $setting ) );
+            }
+        }
+
+        foreach ($this->validations->should_be as $setting => $allowed) {
+            if(defined($setting) && !in_array(constant($setting), $allowed)) {
+                return new WP_Error( 'multisite-smtp', sprintf( __( '%s is invalid. It should be one of these values: "%s"', 'multisite-smtp' ), $setting, implode('" , "',$allowed) ) );
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Hook PHP Mailer to use our SMTP settings
+     */
+    public function mailer( $phpmailer ) {
+
+        //debug?
+        if(defined('GLOBAL_SMTP_DEBUG') && GLOBAL_SMTP_DEBUG ) $phpmailer->SMTPDebug = true;
+
+        //preset
+        $phpmailer->Mailer = "smtp";
+        $phpmailer->SMTPAuth = true;
+
+        //required
+        $phpmailer->Host = GLOBAL_SMTP_HOST;
+        $phpmailer->Username = GLOBAL_SMTP_USER;
+        $phpmailer->Password = GLOBAL_SMTP_PASSWORD;
+
+        //assumed
+        $phpmailer->From = GLOBAL_SMTP_FROM;
+        $phpmailer->FromName = GLOBAL_SMTP_FROM_NAME;
+        $phpmailer->Port = GLOBAL_SMTP_PORT;
+        $phpmailer->SMTPSecure = GLOBAL_SMTP_SECURE;
+        $phpmailer->AuthType = GLOBAL_SMTP_AUTH_TYPE;
+
+        //Optional
+        $phpmailer->Sender = defined('GLOBAL_SMTP_RETURN_PATH') ? GLOBAL_SMTP_RETURN_PATH : GLOBAL_SMTP_FROM;
+
+        if(defined('GLOBAL_SMTP_REPLYTO_FROM')) {
+            $phpmailer->AddReplyTo(GLOBAL_SMTP_REPLYTO_FROM, defined('GLOBAL_SMTP_REPLYTO_FROM_NAME') ? GLOBAL_SMTP_REPLYTO_FROM_NAME : GLOBAL_SMTP_FROM_NAME);
+        }
+    }
 }
+
+/**
+ * Fire it up.
+ */
+Multisite_SMTP::launch();
